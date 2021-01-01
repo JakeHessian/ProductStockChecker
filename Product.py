554e19878feacf6f3301d6f2fcd02347dde678ba
JakeHessian <jake.124124@gmail.com>
2021-01-01 14:24:09 -0500

    Trouble with Bestbuy links, may need to use their API

--- Product.py
+++ Product.py
@@ -1,12 +1,18 @@
 '''
 Product class for organizing information better :)
 '''
-
+import webbrowser as wb
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as BS
+from datetime import datetime
 class Product:
     def __init__(self, productName, productWebsite, productURL):
         self.productName = productName
         self.productURL = productURL
         self.productWebsite = productWebsite
+    def __str__(self):
+        #"Vector3([{0},{1},{2}])".format(self.x, self.y, self.z)
+        return  "Name: {0} Website: {1} URL: {2}".format(self.productName, self.productWebsite, self.productURL)
 
     def getProductName(self):
         return self.productName
@@ -27,10 +33,28 @@ class Product:
         self.productWebsite = productWebsite
         
     def check(self):
-        if productWebsite.lower() in "newegg":
-            getStockNewegg(productURL)
-        elif productWebsite.lower() in "memoryexpress":
-            getStockMemoryExpress(productURL)
-        else:
-            print("Error: website name not found.")
-            return
+        #print(self)
+        page = urlopen(self.productURL)
+        #print("URL oppened")
+        html = page.read().decode("utf-8")
+        #print("HTML decoded")
+        soup = BS(html, "html.parser")
+        #print("SOUP BSed")
+        pageString = cleanString(soup.text)
+        
+        print(datetime.now().strftime("%H:%M:%S"), self.productName, self.productWebsite, end=" ")
+        if "outofstock" in pageString.lower() or "soldout" in pageString.lower():
+            print("Out of stock. :(")
+            return False
+        wb.open(self.productURL, new=2)
+        print("STOCK FOUND!")
+        alarm()
+        return True
+
+def cleanString(string):
+    newString = ""
+    vetoList = " \t\n"
+    for char in string:
+        if char not in vetoList:
+            newString = newString + char
+    return newString
--- main.py
+++ main.py
@@ -1,105 +1,40 @@
-from urllib.request import urlopen
 from pygame import mixer
-from datetime import datetime
-import time
-import webbrowser as wb
-from bs4 import BeautifulSoup as BS
 from Product import Product
+import time
+
 products = []  # product list
 mixer.init()
 mixer.music.load("found.mp3")
 
 def loadProducts(fileName):
-    inFile = open("urls.txt", "r")
+    inFile = open(fileName, "r")
     for line in inFile:
         l = line.split(",")
-        products.append(Product(l[0], l[1], l[3]))
-
-
-def cleanString(string):
-    newString = ""
-    vetoList = " \t\n"
-    for char in string:
-        if char not in vetoList:
-            newString = newString + char
-    return newString
-
-
-def getStockNewegg(URL):
-    page = urlopen(URL)
-    html = page.read().decode("utf-8")
-    soup = BS(html, "html.parser")
-    productInventory = soup.find(
-        "div", {"class": "product-inventory"}).get_text()
-    if productInventory in " OUT OF STOCK.":
-        return False
-    return True
-
-
-def getStockMemoryExpress(URL):
-    page = urlopen(URL)
-    html = page.read().decode("utf-8")
-    soup = BS(html, "html.parser")
-    productInventory = soup.find(
-        "div", {"class": "c-capr-inventory__availability"}).get_text()
-    productInventory = cleanString(productInventory)
-    if "OutofStock" in productInventory:
-        return False
-    return True
-
+        products.append(Product(l[0], l[1], l[2]))
+    #print(products)
 
 def alarm():
     for x in range(5):
         mixer.music.play()
         time.sleep(2)
 
-
-def stockChecker(choice):
-    if choice in "newEgg":
-        print(datetime.now().strftime("%H:%M:%S") +
-              " Checking NewEgg stocking info...", end="")
-        if getStockNewegg(URL):
-            wb.open(URL, new=2)
-            alarm()
-            return True
-        else:
-            print("Stock not found :(")
-
-    elif choice in "memoryExpress":
-        print(datetime.now().strftime("%H:%M:%S") +
-              " Checking MemoryExpress stocking info...", end="")
-        if getStockNewegg(URL):
-            wb.open(memExpressURL, new=2)
-            alarm()
-            return True
-        else:
-            print("Stock not found :(")
-    else:
-        print("Choice not found.")
-    return False
-
-
 def stockHelper():
     stockFlag = False
     while True:
-        time.sleep(60)
         for product in products:
             time.sleep(1)
             stockFlag = product.check()
-
-
-        stockFlag = stockChecker("newEgg")
-        time.sleep(1)
-        stockFlag = stockChecker("memoryExpress")
-        if stockFlag == True:
-            return
+            if stockFlag == True:
+                return
+        time.sleep(60)
 
 
 def main():
     print("Starting")
     # mixer.music.play()
+    loadProducts("urls.txt")
     stockHelper()
-    print("Stock found!\nprogram exiting")
+    print("Program exiting")
 
 
 if __name__ == "__main__":
